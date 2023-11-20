ba09393aa6c1ac9f899e28d357cce7cc8385ecc7
Leo Bottaro <github@leobottaro.com>
2023-11-20 16:00:15 -0300

    Basic Window

---
+++ GlSharp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GlSharp;
+
+using Window window = new(800, 600, "MyGame");
+window.Run();
---
+++ GlSharp/Window.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace GlSharp;
+
+internal class Window : GameWindow
+{
+    public Window(int width, int height, string title)
+        : base(GameWindowSettings.Default, new NativeWindowSettings()
+        {
+            Size = (width, height),
+            Title = title
+        })
+    { }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+        base.OnUpdateFrame(args);
+        if (this.KeyboardState.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+    }
+}

19c79dfb588f890f8f32a371cd1378a075495781
Leo Bottaro <github@leobottaro.com>
2023-11-21 18:48:40 -0300

    Rotating cube

--- GlSharp/Engine.cs
+++ GlSharp/Engine.cs
@@ -29,6 +29,7 @@ internal class Engine : GameWindow
     {
         PrintHardwareSupport();
         GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        GL.Enable(EnableCap.DepthTest);
         this.basicShape = new();
     }
 
@@ -40,7 +41,7 @@ internal class Engine : GameWindow
 
     private void EngineRenderFrame(FrameEventArgs obj)
     {
-        GL.Clear(ClearBufferMask.ColorBufferBit);
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
         this.basicShape?.Draw();
         SwapBuffers();
     }
--- GlSharp/Models/Basic.cs
+++ GlSharp/Models/Basic.cs
@@ -1,4 +1,6 @@
-﻿using GlSharp.Shaders;
+﻿using System.Diagnostics;
+
+using GlSharp.Shaders;
 using GlSharp.Textures;
 
 using OpenTK.Graphics.OpenGL;
@@ -8,26 +10,77 @@ namespace GlSharp.Models;
 
 internal class Basic : IDisposable
 {
+    private readonly Stopwatch sw;
     private readonly Shader shader;
     private readonly Texture texture1;
     private readonly Texture texture2;
     private readonly int vertexHandle;
     private readonly int vao; // Vertex Array Object
     private readonly int ebo; // Element Buffer Object
+
+    // Cube:
     private readonly float[] vertices = {
-      // positions        // colors          // Texture coordinates
-      0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 1.0f,  1.0f, 1.0f,  // top right
-      0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 1.0f,  1.0f, 0.0f,  // bottom right
-     -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f,  // bottom left
-     -0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 1.0f,  0.0f, 1.0f   // top left
+         // positions         // colors          // Texture coordinates
+        -0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f
     };
-    private readonly uint[] indices = {
-        0, 1, 3,   // first triangle
-        2, 3, 1,   // second triangle
-    };
+
+    // Square:
+    //private readonly float[] vertices = {
+    //  // positions        // colors          // Texture coordinates
+    //  0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 1.0f,  1.0f, 1.0f,  // top right
+    //  0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 1.0f,  1.0f, 0.0f,  // bottom right
+    // -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f,  // bottom left
+    // -0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 1.0f,  0.0f, 1.0f   // top left
+    //};
+    //private readonly uint[] indices = {
+    //    0, 1, 3,   // first triangle
+    //    2, 3, 1,   // second triangle
+    //};
 
     public Basic()
     {
+        sw = Stopwatch.StartNew();
+
         // Vertex Array Object - Bundles the data into a single buffer
         this.vao = GL.GenVertexArray();
         GL.BindVertexArray(this.vao);
@@ -45,13 +98,12 @@ internal class Basic : IDisposable
         GL.EnableVertexAttribArray(2);
 
         // Element Buffer Object - How to draw the vertices
-        this.ebo = GL.GenBuffer();
-        GL.BindBuffer(BufferTarget.ElementArrayBuffer, this.ebo);
-        GL.BufferData(BufferTarget.ElementArrayBuffer, this.indices.Length * sizeof(uint), this.indices, BufferUsageHint.StaticDraw);
+        //this.ebo = GL.GenBuffer();
+        //GL.BindBuffer(BufferTarget.ElementArrayBuffer, this.ebo);
+        //GL.BufferData(BufferTarget.ElementArrayBuffer, this.indices.Length * sizeof(uint), this.indices, BufferUsageHint.StaticDraw);
 
         // transformation
 #warning todo: refactor this
-        Matrix4 model = Matrix4.CreateRotationX(MathHelper.DegreesToRadians(-55.0f));
         Matrix4 view = Matrix4.CreateTranslation(0.0f, 0.0f, -3.0f);
         Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45.0f), (float)800 / 800, 0.1f, 100.0f);
 
@@ -64,7 +116,6 @@ internal class Basic : IDisposable
         this.shader.Use();
         this.shader.SetInt("texture1", 0); // unit 0
         this.shader.SetInt("texture2", 1); // unit 1
-        this.shader.SetMat4("model", model); 
         this.shader.SetMat4("view", view);
         this.shader.SetMat4("projection", projection);
     }
@@ -82,13 +133,18 @@ internal class Basic : IDisposable
 
     public void Draw()
     {
+#warning todo: move the update below to an update function
+        Matrix4 model = Matrix4.CreateRotationX((float)MathHelper.DegreesToRadians(sw.Elapsed.TotalSeconds*10));
+
         GL.BindVertexArray(this.vao);
         this.shader.Use();
+        this.shader.SetMat4("model", model);
         this.texture1.Use(TextureUnit.Texture0);
         this.texture2.Use(TextureUnit.Texture1);
         SetTextureParameters();
 
-        GL.DrawElements(PrimitiveType.Triangles, this.indices.Length, DrawElementsType.UnsignedInt, 0);
+        GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
+        //GL.DrawElements(PrimitiveType.Triangles, this.indices.Length, DrawElementsType.UnsignedInt, 0);
     }
 
     protected virtual void Dispose(bool disposing)

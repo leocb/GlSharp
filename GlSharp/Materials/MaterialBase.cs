389a7b40384a7d38342661ef2cd7a64e6563d57b
Leo Bottaro <github@leobottaro.com>
2024-06-07 01:41:30 -0300

    Clean Materials code

--- GlSharp/Materials/MaterialBase.cs
+++ GlSharp/Materials/MaterialBase.cs
@@ -11,19 +11,13 @@ public abstract class MaterialBase : IMaterial
 
     protected int[] textureHandles;
 
-    protected MaterialBase(string vertexShader, string fragShader, int[]? textures = null)
+    protected MaterialBase(string vertexShader, string fragShader)
     {
         Program = new Shaders.Program(vertexShader, fragShader);
-        textureHandles = textures ?? Array.Empty<int>();
     }
 
     public virtual void Use()
     {
         Program.Use();
-
-        for (int i = 0; i < textureHandles.Length; i++)
-        {
-            TextureLoader.Use(textureHandles[i], TextureUnit.Texture0 + i);
-        }
     }
 }
--- GlSharp/Materials/PhongTexturedMaterial.cs
+++ GlSharp/Materials/PhongTexturedMaterial.cs
@@ -8,35 +8,27 @@ using OpenTK.Mathematics;
 namespace GlSharp.Materials;
 public class PhongTexturedMaterial : MaterialBase
 {
-    public PhongTexturedMaterial(SpotLight flashlight, PointLight[] lamps, SunLight sun, string DiffuseTexName)
+    public PhongTexturedMaterial(SpotLight flashlight, PointLight[] lamps, SunLight sun)
         : base("Basic.vert", "PhongTextured.frag")
     {
-
-        string[] texNames = DiffuseTexName.Split('.');
-
-        int diffuseTextHandle = TextureLoader.Load($"{texNames[0]}.{texNames[1]}");
-        int SpecularTextHandle = TextureLoader.Load($"{texNames[0]}_specular.{texNames[1]}");
-
-        textureHandles = new int[] {
-            diffuseTextHandle,
-            SpecularTextHandle
-        };
-
         GlTools.TsGlCall(() =>
         {
             Program.Use();
 
             // Flashlight
-            Program.SetVec3("spotLight.position", flashlight.Position);
-            Program.SetVec3("spotLight.direction", flashlight.Direction);
-            Program.SetFloat("spotLight.cutOffStart", flashlight.CutOffStart);
-            Program.SetFloat("spotLight.cutOffEnd", flashlight.CutOffEnd);
-            Program.SetVec3("spotLight.ambient", flashlight.AmbientColor);
-            Program.SetVec3("spotLight.diffuse", flashlight.DifuseColor);
-            Program.SetVec3("spotLight.specular", flashlight.SpecularColor);
-            Program.SetFloat("spotLight.Kc", flashlight.KConstant);
-            Program.SetFloat("spotLight.Kl", flashlight.KLinear);
-            Program.SetFloat("spotLight.Kq", flashlight.KQuadratic);
+            if (flashlight is not null)
+            {
+                Program.SetVec3("spotLight.position", flashlight.Position);
+                Program.SetVec3("spotLight.direction", flashlight.Direction);
+                Program.SetFloat("spotLight.cutOffStart", flashlight.CutOffStart);
+                Program.SetFloat("spotLight.cutOffEnd", flashlight.CutOffEnd);
+                Program.SetVec3("spotLight.ambient", flashlight.AmbientColor);
+                Program.SetVec3("spotLight.diffuse", flashlight.DifuseColor);
+                Program.SetVec3("spotLight.specular", flashlight.SpecularColor);
+                Program.SetFloat("spotLight.Kc", flashlight.KConstant);
+                Program.SetFloat("spotLight.Kl", flashlight.KLinear);
+                Program.SetFloat("spotLight.Kq", flashlight.KQuadratic);
+            }
 
             // Lamps
             for (int i = 0; i < 4; i++)
@@ -58,9 +50,7 @@ public class PhongTexturedMaterial : MaterialBase
 
             // Textures
             // int value is defined by the order which the texture was added to the textureHandles array above
-            Program.SetInt("material.diffuse", 0);
-            Program.SetInt("material.specular", 1);
-            Program.SetFloat("material.shininess", 0.8f * 128.0f);
+            Program.SetFloat("material.shininess", 0.9f * 128.0f);
         });
     }
 
